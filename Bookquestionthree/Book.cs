cb7d8c53d1928edeedf65b95503559da0787a6f1
Rahul Khatri <forever_rahul16@hotmail.com>
2021-09-28 14:41:33 +1300

    Add project files.

---
+++ Bookquestionthree/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookquestionthree
+{
+    class Book
+    {
+        //These three below are auto-implemented properties.
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+
+        //This below is parameterless constructor.
+        public Book()
+        {
+            Console.WriteLine("*** ENTER NEW BOOK DETAILS BELOW ***"); //The sententce or piece of program on the left is Welcoming message.
+        }
+
+        //This below is paramteriesed constructor which has three parameters (title, author, publisher).
+        public Book (string tle, string au, string pub)
+        {
+            tle = Title;
+            au = Author;
+            pub = Publisher;
+        }
+
+
+    }
+}
---
+++ Bookquestionthree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookquestionthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The thing below is new object created.
+            Book b1 = new Book();
+
+            Console.Write("\nEnter Book Title: ");
+            b1.Title = Console.ReadLine();
+
+            Console.Write("Enter Book Author: ");
+            b1.Author = Console.ReadLine();
+
+            Console.Write("Enter Book Publisher: ");
+            b1.Publisher = Console.ReadLine();
+
+            Console.WriteLine("\n******************************************");
+            Console.WriteLine("The following new book has been processed:");
+            Console.WriteLine("Book Title: {0}\nAuthor: {1}\nPublisher: {2}", b1.Title, b1.Author, b1.Publisher);
+
+            Console.ReadKey();
+
+            //This programme is very useful in Book stores or in public libraries. The developer of this programme is Rahul Khatri.
+        }
+    }
+}
